4a94bcf94ba672389b35c7c6ad6f7566e0a2f003
Spain Train <michael.spainhower@opower.com>
2014-12-19 14:05:24 -0500

    Use 4 space tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var _ = require('ls-lodash'),
-  jshint = require('gulp-jshint'),
-  jscs = require('gulp-jscs'),
-  gulp = require('gulp'),
-  lsConfigs = require('./');
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    gulp = require('gulp'),
+    lsConfigs = require('./');
 
 gulp.task('default', _.noop);
 
 gulp.task('test', function() {
-  gulp.src(['*.js'])
-    .pipe(jshint(lsConfigs.jshintrc))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jscs(lsConfigs.jscsrc));
+    gulp.src(['*.js'])
+        .pipe(jshint(lsConfigs.jshintrc))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(jscs(lsConfigs.jscsrc));
 });
